860e3d2d5c4639591a18bf908fea8e9eefecb23a
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-24 22:01:42 +0530

    cache repository write function

--- repository/cache.go
+++ repository/cache.go
@@ -1,6 +1,14 @@
 package repository
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/Abhishek2010dev/Caching-Proxy-With-Redis/models"
+	"github.com/redis/go-redis/v9"
+)
 
 type CacheRepository struct {
 	client *redis.Client
@@ -8,4 +16,18 @@ type CacheRepository struct {
 
 func NewCacheRepository(client *redis.Client) *CacheRepository {
 	return &CacheRepository{client}
+}
+
+func (c *CacheRepository) StoreCachedEntry(ctx context.Context, key string, cachedEntry *models.CachedEntry, expiration time.Duration) error {
+	data, err := json.Marshal(cachedEntry)
+	if err != nil {
+		return fmt.Errorf("failed to marshal cached entry: %w", err)
+	}
+
+	_, err = c.client.Set(ctx, key, data, expiration).Result()
+	if err != nil {
+		return fmt.Errorf("failed to set cache entry: %w", err)
+	}
+
+	return nil
 }

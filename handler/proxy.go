52b99757039d2e279cc3765ec5c12c1845930498
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-25 11:22:55 +0530

    proxy server

---
+++ handler/proxy.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Abhishek2010dev/Caching-Proxy-With-Redis/repository"
+	"github.com/redis/go-redis/v9"
+)
+
+type Proxy struct {
+	origin     string
+	cache      *repository.CacheRepository
+	expiration time.Duration
+}
+
+func NewProxy(origin string, expiration time.Duration, redisClient *redis.Client) *Proxy {
+	return &Proxy{
+		origin:     origin,
+		expiration: expiration,
+		cache:      repository.NewCacheRepository(redisClient),
+	}
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	CACHE_KEY := fmt.Sprintf("%s:%s", r.Method, r.URL)
+	val, err := p.cache.GetCachedEntry(r.Context(), CACHE_KEY)
+}
--- models/cache.go
+++ models/cache.go
@@ -1,12 +1,10 @@
 package models
 
 import (
-	"net/http"
 	"time"
 )
 
 type CachedEntry struct {
-	Response     *http.Response `json:"response"`
-	ResponseBody []byte         `json:"responseBody"`
-	Created      time.Time      `json:"created"`
+	ResponseBody []byte    `json:"responseBody"`
+	Created      time.Time `json:"created"`
 }

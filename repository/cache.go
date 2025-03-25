1ef9202fc94f04e92d677e4fd131e51982aff420
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-25 09:58:43 +0530

    removing error for error type

--- repository/cache.go
+++ repository/cache.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/Abhishek2010dev/Caching-Proxy-With-Redis/models"
@@ -21,12 +20,12 @@ func NewCacheRepository(client *redis.Client) *CacheRepository {
 func (c *CacheRepository) StoreCachedEntry(ctx context.Context, key string, cachedEntry *models.CachedEntry, expiration time.Duration) error {
 	data, err := json.Marshal(cachedEntry)
 	if err != nil {
-		return fmt.Errorf("failed to marshal cached entry: %w", err)
+		return err
 	}
 
 	_, err = c.client.Set(ctx, key, data, expiration).Result()
 	if err != nil {
-		return fmt.Errorf("failed to set cache entry: %w", err)
+		return err
 	}
 
 	return nil
@@ -35,12 +34,12 @@ func (c *CacheRepository) StoreCachedEntry(ctx context.Context, key string, cach
 func (c *CacheRepository) GetCachedEntry(ctx context.Context, key string) (*models.CachedEntry, error) {
 	val, err := c.client.Get(ctx, key).Bytes()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get cached entry: %w", err)
+		return nil, err
 	}
 
 	var cachedEntry models.CachedEntry
 	if err := json.Unmarshal(val, &cachedEntry); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal cached entry: %w", err)
+		return nil, err
 	}
 
 	return &cachedEntry, nil

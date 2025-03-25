b70fa242cbd69805b86d9a20a43f8eb157bafec7
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-25 11:52:01 +0530

    ResponsedWithHeader function

--- handler/proxy.go
+++ handler/proxy.go
@@ -3,9 +3,12 @@ package handler
 import (
 	"errors"
 	"fmt"
+	"log"
+	"maps"
 	"net/http"
 	"time"
 
+	"github.com/Abhishek2010dev/Caching-Proxy-With-Redis/models"
 	"github.com/Abhishek2010dev/Caching-Proxy-With-Redis/repository"
 	"github.com/redis/go-redis/v9"
 )
@@ -26,11 +29,23 @@ func NewProxy(origin string, expiration time.Duration, redisClient *redis.Client
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	CACHE_KEY := fmt.Sprintf("%s:%s", r.Method, r.URL)
-	val, err := p.cache.GetCachedEntry(r.Context(), CACHE_KEY)
 
+	val, err := p.cache.GetCachedEntry(r.Context(), CACHE_KEY)
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
-
+			ResponsedWithHeader(w, val, "HIT", CACHE_KEY)
+			return
 		}
+		log.Printf("Failed to get cache entry: %s", err.Error())
+		http.Error(w, "Samething went wrong", http.StatusInternalServerError)
+		return
 	}
+}
+
+func ResponsedWithHeader(w http.ResponseWriter, cacheEntry *models.CachedEntry, cacheHeader, KEY string) {
+	log.Printf("Cache : %s %s \n", cacheHeader, KEY)
+	w.Header().Set("X-Cache", cacheHeader)
+	w.WriteHeader(cacheEntry.StatusCode)
+	maps.Copy(w.Header(), cacheEntry.Header)
+	w.Write(cacheEntry.ResponseBody)
 }

104283c7111f14ad478623a9c614ff5ffafeadd8
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-25 12:20:32 +0530

    if cache does exits code

--- handler/proxy.go
+++ handler/proxy.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"maps"
 	"net/http"
@@ -33,7 +34,7 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	val, err := p.cache.GetCachedEntry(r.Context(), CACHE_KEY)
 	if err != nil && !errors.Is(err, redis.Nil) {
 		log.Printf("Failed to get cache entry: %s", err.Error())
-		http.Error(w, "Samething went wrong", http.StatusInternalServerError)
+		http.Error(w, "Error Getting cache entry", http.StatusInternalServerError)
 		return
 	}
 
@@ -43,6 +44,39 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// If cache does not exits
+	fmt.Printf("Cache Not Present for key : %s \n", CACHE_KEY)
+	originUrl := p.origin + r.URL.String()
+	resp, err := http.Get(originUrl)
+	if err != nil {
+		log.Printf("Failed to forward request: %s", err.Error())
+		http.Error(w, "Error Forwarding Request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to forward request body: %s", err.Error())
+		http.Error(w, "Error Forwarding Request Body", http.StatusInternalServerError)
+		return
+	}
+
+	entry := models.CachedEntry{
+		StatusCode:   resp.StatusCode,
+		Header:       resp.Header,
+		ResponseBody: body,
+		Created:      time.Now(),
+	}
+	err = p.cache.StoreCachedEntry(r.Context(), CACHE_KEY, &entry, p.expiration)
+	if err != nil {
+		log.Printf("Failed to set cache entry: %s", err.Error())
+		http.Error(w, "Error Setting cache entry", http.StatusInternalServerError)
+		return
+
+	}
+
+	ResponsedWithHeader(w, &entry, "MISS", CACHE_KEY)
 }
 
 func ResponsedWithHeader(w http.ResponseWriter, cacheEntry *models.CachedEntry, cacheHeader, KEY string) {
